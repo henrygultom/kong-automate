9653a2ccbfbe88a5eabe309ecb884a86ea461ef8
henrygultom <53945023+henrygultom@users.noreply.github.com>
2022-09-02 15:10:56 +0700

    Add files via upload

--- test/kong_migration_check.sh
+++ test/kong_migration_check.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
 
+PG_HOST=$1
+PG_CMD=/usr/local/bin/psql.sh
+
+function print_schema_migrations() {
+    echo "&&&&&&&&  $(date)"
+
+    echo "Print kong.schema_migrations table"
+    echo "select * from schema_migrations;" | sudo ${PG_CMD}
+
+    echo "Print count of entries in schema_migrations table"
+    echo "select count(*) from schema_migrations;" | sudo ${PG_CMD}
+}
+
+function print_tables_count() {
+    echo "&&&&&&&&  $(date)"
+
+    for table in apis consumers plugins
+    do
+        echo "Print count of entries in kong.${table} table"
+        echo "select count(*) from ${table};" | sudo ${PG_CMD}
+    done
+}
+
+
+# Main
+print_schema_migrations | tee -a $(dirname $0)/migrations.log
+print_tables_count | tee -a $(dirname $0)/tables.log
